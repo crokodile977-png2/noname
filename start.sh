ee4918ffd07b465f8c7bef98b570c04a6cf891e6
crokodile977-png2 <wrrgfgtj2@gmail.com>
2026-08-23 22:12:49 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,5 @@
 # Запускаем Xray на внутреннем порту 8080 в фоновом режиме (&)
 xray run -c /etc/xray/config.json &
 
-# Запускаем Caddy, который примет внешний трафик на порту Railway ($PORT)
-# exec делает его главным процессом контейнера, чтобы Railway видел, что он жив
+# Запускаем Caddy как главный процесс контейнера (exec)
 exec caddy run --config /etc/caddy/Caddyfile
