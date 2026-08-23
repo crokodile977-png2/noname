2e10e80a26c7c35ce8443020e34a2daaecce30da
crokodile977-png2 <wrrgfgtj2@gmail.com>
2026-08-23 22:04:35 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Заменяем порт 8080 на тот, который выдаст Railway ($PORT)
 sed -i "s/8080/${PORT}/g" /etc/xray/config.json
-# Запускаем Xray (ядро V2Ray)
-xray run -c /etc/xray/config.json
+
+# Запускаем Xray (exec передает управление процессу, чтобы он корректно работал в контейнере)
+exec xray run -c /etc/xray/config.json
