c5eb47235416bedab470710f4e7bfe32e5c5a74d
crokodile977-png2 <wrrgfgtj2@gmail.com>
2026-08-23 22:33:26 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-# Запускаем Xray на внутреннем порту 8080 в фоновом режиме (&)
+# 1. Заменяем PORT_PLACEHOLDER на реальный порт, который выдал Railway
+sed -i "s/PORT_PLACEHOLDER/${PORT}/g" /etc/nginx/nginx.conf
+
+# 2. Запускаем Xray в фоновом режиме на порту 8081
 xray run -c /etc/xray/config.json &
 
-# Запускаем Caddy как главный процесс контейнера (exec)
-exec caddy run --config /etc/caddy/Caddyfile
+# 3. Запускаем Nginx в главном потоке (daemon off; нужен для Docker, чтобы контейнер не падал)
+exec nginx -g "daemon off;"
